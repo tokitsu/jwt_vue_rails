a0be3131a870d3cb0a2153309a69536b967d088c
tokitsu <toki2222son@yahoo.co.jp>
2021-07-01 21:13:24 +0900

    add login with jwt

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'refresh', controller: :refresh, action: :create
+  post 'signin', controller: :signin, action: :create
+  post 'signup', controller: :signup, action: :create
+  delete 'signin', controller: :signin, action: :destroy
+
+  resources :todos
 end

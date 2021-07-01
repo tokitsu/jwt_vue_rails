9d659466362f1649bd1d97648757d2095fb23b13
tokitsu <toki2222son@yahoo.co.jp>
2021-07-01 22:49:43 +0900

    wrote README

--- todos-vue/src/backend/axios/index.js
+++ todos-vue/src/backend/axios/index.js
@@ -31,7 +31,6 @@ securedAxiosInstance.interceptors.request.use(config => {
 
 securedAxiosInstance.interceptors.response.use(null, error => {
   if (error.response && error.response.config && error.response.status === 401) {
-    // In case 401 is caused by expired access cookie - we'll do refresh request
     return plainAxiosInstance.post('/refresh', {}, { headers: { 'X-CSRF-TOKEN': localStorage.csrf } })
       .then(response => {
         localStorage.csrf = response.data.csrf
@@ -43,7 +42,7 @@ securedAxiosInstance.interceptors.response.use(null, error => {
       }).catch(error => {
         delete localStorage.csrf
         delete localStorage.signedIn
-        // redirect to signin in case refresh request fails
+
         location.replace('/')
         return Promise.reject(error)
       })
